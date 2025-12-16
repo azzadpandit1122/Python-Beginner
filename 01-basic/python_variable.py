61cdcdfd6c7903f3b6a0491f1cc124b2caa29467
shalu panit <shalupandit2025@gmail.com>
2025-12-17 01:06:23 +0530

    in variables

--- 01-basic/python_variable.py
+++ 01-basic/python_variable.py
@@ -46,3 +46,80 @@ print(a)
 a = 4
 A = 23456677899
 print(a)
+
+myvar = "john"
+my_var = "dillan"
+_my_var = "world"
+print(myvar)
+print(my_var)
+print(_my_var)
+
+
+
+myVar = "jandi"
+print(myVar)
+
+MyVar = "goyal"
+print(MyVar)
+
+x , y , z = "fruits" , "vegetable" , "animals"
+
+print(x)
+
+# x , y , z = "fruits" , "vegetable" , "animals"
+
+# print(x=y)
+
+x = y = z = "lily"
+print(x)
+print(y)
+print(z)
+
+
+
+fruits = ["apple" , "banana" , "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+x = "awesome"
+y = "bad"
+z = "goal"
+
+print(x , y , z)
+
+x = "awesome"
+y = "bad"
+z = "goal"
+
+print(x + y + z)
+
+
+x = 5
+y = 7
+
+print(x + y)
+
+x = 5
+y = "john"
+
+print(x , y)
+
+
+x = "excellent"
+
+def myfunc():
+     print("python is " + x)
+
+myfunc()
+
+
+
+x = "excellent"
+def myfunc():
+     x = "fantastic"
+     print("python is " + x)
+myfunc()
+#  print("python is " + x)
