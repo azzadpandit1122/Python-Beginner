1527497be938841cb095ab2b5cf59e15af586ef1
shalu panit <shalupandit2025@gmail.com>
2025-12-17 00:06:01 +0530

    number code

---
+++ 01-basic/python_number.py
@@ -0,0 +1,13 @@
+print(3)
+print(30)
+print(300)
+print(3000)
+
+
+print(3 * 5)
+
+print(20 * 5)
+
+print("I am", 18, "year old") #this is a comment 
+
+print(3); print(30); print(300); print(3000)
--- 01-basic/python_syntax.py
+++ 01-basic/python_syntax.py
@@ -1,2 +1,25 @@
 print("python indentation")
 print("Indentation refers to the spaces at the beginning of a code line")
+
+if 10 > 5:
+   print("Ten is greater than five!")
+   
+   print("Ten is greater than five!")
+
+x = 5
+y = "welcome in python"
+
+print(x)
+print(y)
+
+print("welcome in python")
+print("hello world")
+print("good night")
+
+
+if 100 < 30:
+    print("100 is less than 30")
+
+    print("hello world", end=" ")
+
+    
