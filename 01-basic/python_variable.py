49f25e07f7d7baf522eef98fd17d47137b1470d1
shalu panit <shalupandit2025@gmail.com>
2025-12-17 00:27:32 +0530

    variable_test

---
+++ 01-basic/python_variable.py
@@ -0,0 +1,48 @@
+y = 5
+y = "hello"
+print(y)
+
+x = int(5)
+y = str(8)
+z = float(10)
+print(y)
+
+
+x = int(5)
+y = int(8)
+z = int(10)
+print(y)
+
+
+
+x = 5
+y = "john"
+print(int(x))
+print(str(y))
+
+
+x = 5
+y = "john"
+print(x)
+print(str(y))
+
+x = "john"
+x = 6
+
+print(x)
+
+a = 4
+A = "sally"
+print(a)
+
+a = 4
+A = "sally"
+print(A)
+
+a = "python"
+A = "sally"
+print(a)
+
+a = 4
+A = 23456677899
+print(a)
