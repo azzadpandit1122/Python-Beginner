1cc9d81b6a01fcf2d1c8d158e8146f9c46fa3605
shalu panit <shalupandit2025@gmail.com>
2025-12-17 23:46:59 +0530

    syntax.py

--- 01-basic/python_string.py
+++ 01-basic/python_string.py
@@ -1,1 +1,15 @@
 print("my profession 'python'")
+
+
+a = "And then the day came,when the risk to remain tight in a bud was more painful than the risk it took to blossom."
+
+print(a)
+
+b = "hello , world"
+print(b[7:15])
+
+p = "Hello , World"
+print(p.upper())
+
+q = "Hello , World"
+print(q.lower())
