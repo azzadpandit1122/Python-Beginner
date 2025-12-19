c7e7c9170f3bbb2164ebfe335926500a0beec44d
shalu panit <shalupandit2025@gmail.com>
2025-12-19 17:21:29 +0530

    operater.py

---
+++ 01-basic/python_operater.py
@@ -0,0 +1,96 @@
+# print(10 +65)
+
+# sum1 = 57 + 576
+# sum2 = 4627 + 5
+# print(sum1 + sum2)
+
+
+# a = 64
+# b = 2
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+# print(a // b)
+
+
+# a = 64
+# b = 5
+# print(a //b)
+
+
+
+# x = 5
+# y = 3
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+
+
+
+# x = 5
+
+# print(1 < x < 10)
+
+# print(1 < x and x < 10)
+
+# x =6
+# print(not(x < 5 and x > 1))
+
+
+# x = ["vegetable" , "fruits"]
+# y = ["vegetable" , "fruits"]
+# z = x
+# print(x is not y)
+# print(x is z)
+# print(x == y)
+
+# x =  [1 , 2, 3]
+# y = [1 , 2, 3]
+# p = x
+# print(x is not y)
+# print(p is x )
+# print(x == y)
+
+
+# fruits = ["apple", "banana", "orange"]
+# print("fruit" in fruits)
+
+
+# fruits = ["apple", "banana", "orange"]
+# print("fruit" not in fruits)
+
+
+# vegetable = ['potato', "tomato", "cabbage"]
+# fruits = ["pineapple", "pomogrante", "banana"]
+# print(vegetable + fruits)
+# print(vegetable == fruits)
+# print(vegetable is  fruits)
+# print(vegetable is not fruits)
+# print(vegetable in fruits)
+# print(vegetable not in fruits)
+# print("banana" in fruits)
+# print("potato" in vegetable)
+# print("cabbage" in fruits)
+# print("banana" not in vegetable)
+
+# text = "Hello World"
+
+# print("H" in text)
+# print("Hello World" in text)
+# print("z" not in text)
+
+
+print(6 & 2)
+print (8 & 2)
+print(6 | 2)
+print(6 ^ 2)
+print(6 - 2)
+print(6 << 2)
+print(6 >> 2)
