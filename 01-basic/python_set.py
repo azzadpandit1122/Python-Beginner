7dc4fe7365f2a355da3ad2e5a37ea4672caa4621
shalu panit <shalupandit2025@gmail.com>
2025-12-21 00:56:47 +0530

    set.py

--- 01-basic/python_set.py
+++ 01-basic/python_set.py
@@ -19,5 +19,44 @@ thisset = {"apple", "banana", "cherry", True, False, 1, 2}
 print(type(thisset))
 
 
-thisset = set(("apple", "banana", "cherry", "True", "False", 1, 2))
+# thisset = set(("apple", "banana", "cherry", "True", "False", 1, 2))
+# print(thisset)
+
+thisset = {"apple", "banana", "cherry", "True", "False", 1, 2}
+thisset.add("orange")
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry", "True", "False", 1, 2}
+tropical  = {"mango", "pineapple", "papaya"}
+thisset.update("tropical")
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry", "True", "False", 1, 2}
+tropical  = ["mango", "pineapple", "papaya"]
+thisset.update("tropical")
+print(thisset)
+
+
+thisset  = {"mango", "pineapple", "papaya"}
+thisset.discard("pineapple")
+print(thisset)
+
+
+thisset  = ["mango", "pineapple", "papaya"]
+x = thisset.pop()
+print(x)
+print(thisset)
+
+
+
+thisset  = {"mango", "pineapple", "papaya"}
+thisset.clear()
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+del thisset
+
 print(thisset)
